bfe8e5871627517fcc22d9db4db5a01fffc36c5a
sad0unicon <56435446+sad0unicon@users.noreply.github.com>
2020-06-09 20:28:25 +0300

    lab3

--- lab_03/src/main.cpp
+++ lab_03/src/main.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
-#include "general_tree.h"
-#include "binary_tree.h"
+#include "generator.h"
 using namespace std;
 
-int main() {
-    Bin_Tree<int> tree1{1,2,3,4,2,6,7,2,9};
-    tree1.print();
-
-    tree1.pop(9);
-    tree1.print();
-    tree1.double_pop();
-    tree1.print();
-    tree1.erase(6);
-    tree1.print();
-    if (tree1.operator==(tree1))
-        cout << "\nravno";
-    else cout << "\nneravno";
 
 
-    Tree<int> tr3 {11,22,33,22};
-    tr3.print();
-    auto r = tr3.find(22);
+int main(int argc, char *argv[]) {
+    try {
+    	auto t1 = generate<int>(234); // t1 is std::vector
+    	cout << "<TEST>" << endl;
+    	cout << "\t" << t1[0] << " | " << t1.size() <<  endl;
 
-    tr3.push(r.at(0),5);
-    tr3.push(r.at(1),5);
 
-    tr3.double_pop();
-    tr3.print();
-    r = tr3.find(33);
-    tr3.push(r.at(0),5);
-    tr3.print();
-    if (tr3.operator==(tr3))
-        cout << "\nravno";
-    else cout << "\nneravno";
+    	auto t2 = generate<string>(5);
+    	cout << "<TEST>" << endl;
+    	cout << "\t" << t2[0] << " | " << t2.size() <<  endl;
+
+    	auto t3 = generate<float>(6);
+    	cout << "<TEST>" << endl;
+    	cout << "\t" << t3[0] << " | " << t3.size() << endl;
+    } catch (const std::exception &exp) {
+        cout << exp.what() << endl;
+    }
+
     return 0;
 }
